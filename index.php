3826edbc10644c5cae678c5d799d1f802b6686a7
Kil-Hwan <kilhwanpyun@gmail.com>
2022-05-16 21:11:41 -0400

    trying again

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Updock&display=swap" rel="stylesheet"> 
     <link rel = "stylesheet" href = "styles.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <script src="https://kit.fontawesome.com/3a34c0e717.js" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/3a34c0e717.js" crossorigin="anonymous"></script> -->
     <title>NY BREW</title>
 </head>
 <body>
